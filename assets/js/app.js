73c94b48e42a4240ef0e15ce01d62823a679905d
zekariyas alemu <zekialemu3@gmail.com>
2021-03-03 13:43:35 +0300

    js1function

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,39 @@
 var currentCharacter = 1;
-const apiKey = '3108457009190775';
+const apiKey = '1924494977717607';
+function displayResults(responseJson) {
+    $('#results-list').empty();
+    for (let i=0;i<responseJson.results.length;i++){
+      $('#results-list').append(
+        `<li class="item"> 
+        <h3>${responseJson.results[i].name}</h3>
+        <img src="${responseJson.results[i].image.url}" alt="${responseJson.results[i].name}Image" class="results-img">
+        <ul>
+          <li class="realName">True Identity:<br> ${responseJson.results[i].biography['full-name']} (${responseJson.results[i].appearance.race})</li>
+          <li>Height: <br>${responseJson.results[i].appearance.height}</li>
+          <li>Weight: <br>${responseJson.results[i].appearance.weight}</li>
+        </ul>
+        <h4 class="powerStats">Power Stats</h4>
+        <ul>
+          <li>Intelligence: ${responseJson.results[i].powerstats.intelligence}</li>
+          <li>Strength: ${responseJson.results[i].powerstats.strength}</li>
+          <li>Speed: ${responseJson.results[i].powerstats.speed}</li>
+        </ul>
+        <ul>  
+          <li>Durability: ${responseJson.results[i].powerstats.durability}</li>
+          <li>Power: ${responseJson.results[i].powerstats.power}</li>
+          <li>Combat: ${responseJson.results[i].powerstats.combat}</li>
+        </ul>
+          <button type="button" id="${responseJson.results[i].id}" class="button teamButton js-assoc-button">Super Team(s)</button>
+        <div id="${responseJson.results[i].id}teams" class="hidden">
+        <section id="${responseJson.results[i].id}associates">
+            <ul id="associate-list" class="group">
+            </ul>
+        </section>
+        </div>`
+        ); 
+    let currentCharacter = responseJson.results[i].id
+    teamButton(currentCharacter);
+    }
+    $('#results').removeClass('hidden');
+    $('#js-error-message').empty()
+  };
